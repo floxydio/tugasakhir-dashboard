594121ab1c3e38d59376377d3d0686bb814c84ec
marcellinoputra <marcellinoputra@github.com>
2023-05-18 19:55:57 +0700

    update guru

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,42 +1,43 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import imgIcon from "../assets/icon.jpg";
-import { Avatar, Grid } from "@mui/material";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import imgIcon from '../assets/icon.jpg';
+import { Avatar, Grid } from '@mui/material';
 const drawerWidth = 240;
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import Absensi from "../pages/Absensi";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Absensi from '../pages/Absensi';
+import Guru from '../pages/Guru';
 
 function ResponsiveDrawer(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [data, setData] = React.useState([]);
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   const navigate = useNavigate();
   const [changeNav, setChangeNav] = React.useState(0);
 
   React.useEffect(() => {
     async function fetchDataRefresh() {
       await axios
-        .get("http://103.174.115.58:3000/v1/refresh-token", {
+        .get('http://103.174.115.58:3000/v1/refresh-token', {
           headers: {
-            "x-access-token": token,
+            'x-access-token': token,
           },
         })
         .then((res) => {
@@ -47,25 +48,25 @@ function ResponsiveDrawer(props) {
   }, []);
 
   function logout() {
-    localStorage.removeItem("token");
-    navigate("/sign-in");
+    localStorage.removeItem('token');
+    navigate('/sign-in');
   }
 
   let menu = [
     {
       id: 1,
-      name: "Home",
-      url: "/",
+      name: 'Home',
+      url: '/',
     },
     {
       id: 2,
-      name: "Guru",
-      url: "/guru",
+      name: 'Guru',
+      url: '/guru',
     },
     {
       id: 3,
-      name: "Absensi",
-      url: "/absensi",
+      name: 'Absensi',
+      url: '/absensi',
     },
   ];
 
@@ -108,7 +109,7 @@ function ResponsiveDrawer(props) {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar
         position="fixed"
@@ -123,15 +124,15 @@ function ResponsiveDrawer(props) {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
+            sx={{ mr: 2, display: { sm: 'none' } }}
           >
             <MenuIcon />
           </IconButton>
           <div
             style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
+              display: 'flex',
+              justifyContent: 'space-between',
+              width: '100%',
             }}
           >
             <Typography variant="h6" noWrap component="div">
@@ -139,8 +140,8 @@ function ResponsiveDrawer(props) {
             </Typography>
             <div
               style={{
-                display: "flex",
-                alignItems: "center",
+                display: 'flex',
+                alignItems: 'center',
               }}
             >
               <span
@@ -174,9 +175,9 @@ function ResponsiveDrawer(props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
               width: drawerWidth,
             },
           }}
@@ -186,9 +187,9 @@ function ResponsiveDrawer(props) {
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: "none", sm: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
               width: drawerWidth,
             },
           }}
@@ -200,15 +201,15 @@ function ResponsiveDrawer(props) {
       <Box
         component="main"
         sx={{
-          backgroundColor: "#F0F0F4",
+          backgroundColor: '#F0F0F4',
           flexGrow: 1,
-          height: "100vh",
+          height: '100vh',
           p: 3,
           width: { sm: `calc(100% - ${drawerWidth}px)` },
         }}
       >
         <Toolbar />
-        {changeNav == 2 ? : null }
+        {changeNav == 2 ? <Guru /> : null}
         {changeNav == 3 ? <Absensi /> : null}
       </Box>
     </Box>
