baefb974edce4be285d1d644cc787ce9bd398e26
floxydio <diooktar@gmail.com>
2023-05-20 20:15:58 +0700

    edit

--- src/pages/Absensi.jsx
+++ src/pages/Absensi.jsx
@@ -11,18 +11,36 @@ import BarLoader from "react-spinners/BarLoader.js";
 import { MonthModels } from "../models/Month_models";
 import {
   Button,
+  Chip,
   FormControl,
   InputLabel,
   MenuItem,
   Select,
 } from "@mui/material";
 import "../style/absensi.css";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
 const override = (React.CSSProperties = {
   transform: "translate(-50%, -50%)",
   top: "50%",
   left: "50%",
   position: "absolute",
 });
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 export default function Absensi() {
   const [getMonth, setGetMonth] = React.useState(0);
   const [absenData, setAbsenData] = React.useState([]);
@@ -31,6 +49,9 @@ export default function Absensi() {
   const [month, setFilterMonth] = React.useState("");
   const tableRef = React.useRef(null);
   const [loading, setLoading] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   function filterData() {
     let params = {};
@@ -82,11 +103,26 @@ export default function Absensi() {
           sx={{
             marginTop: 1,
           }}
-          onClick={filterData}
+          onClick={handleOpen}
           variant="contained"
         >
           Absen Manual
         </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
         <div>
           <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
             <InputLabel id="demo-simple-select-label">Bulan</InputLabel>
@@ -153,10 +189,12 @@ export default function Absensi() {
                 <TableCell align="right">Pelajaran</TableCell>
                 <TableCell align="right">Nomor Kelas</TableCell>
                 <TableCell align="right">Keterangan</TableCell>
+                <TableCell align="right">Alasan</TableCell>
                 <TableCell align="right">Hari</TableCell>
                 <TableCell align="right">Bulan</TableCell>
                 <TableCell align="right">Tahun</TableCell>
                 <TableCell align="right">Waktu</TableCell>
+                <TableCell align="right">Action</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -172,11 +210,31 @@ export default function Absensi() {
                   <TableCell align="right">{row.nama_guru}</TableCell>
                   <TableCell align="right">{row.pelajaran_nama}</TableCell>
                   <TableCell align="right">{row.nomor_kelas}</TableCell>
-                  <TableCell align="right">{row.keterangan}</TableCell>
+                  <TableCell align="right">
+                    {row.keterangan === "ABSEN" ? (
+                      <Chip label={row.keterangan} color="success" />
+                    ) : (
+                      <Chip label={row.keterangan} color="error" />
+                    )}
+                  </TableCell>
+                  <TableCell align="right">{row.reason}</TableCell>
+
                   <TableCell align="right">{row.day}</TableCell>
                   <TableCell align="right">{row.month}</TableCell>
                   <TableCell align="right">{row.year}</TableCell>
                   <TableCell align="right">{row.time}</TableCell>
+                  <TableCell align="right">
+                    <Button
+                      className="btn_absen"
+                      sx={{
+                        marginTop: 1,
+                      }}
+                      onClick={handleOpen}
+                      variant="contained"
+                    >
+                      Edit
+                    </Button>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
