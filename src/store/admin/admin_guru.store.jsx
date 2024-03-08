9b6cef187439710664de7ff21408afc6b5a5e176
marcellinoputra <marcellinoputra@github.com>
2024-03-09 02:09:56 +0700

    admin guru store update

--- src/store/admin/admin_guru.store.jsx
+++ src/store/admin/admin_guru.store.jsx
@@ -19,5 +19,29 @@ export const useAdminGuru = create((set, get) => ({
         }
       });
   },
-  createGuru: async () => {}
+  createGuru: async (guru_id, nama, username, status_guru) => {
+    const userAgent = window.navigator.userAgent;
+    await axiosNew
+      .post(
+        "/admin/create-guru",
+        {
+          guru_id: guru_id,
+          nama: nama,
+          username: username,
+          status_guru: status_guru,
+          user_agent: userAgent,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "x-access-token": localStorage.getItem("token"),
+          },
+        }
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          getGuru();
+        }
+      });
+  },
 }));
