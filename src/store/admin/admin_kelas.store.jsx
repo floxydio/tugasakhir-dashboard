72c7502da24b6539b10d920195261f9aae60ca3a
SuisF <enykura394@gmail.com>
2024-03-09 20:22:51 +0700

    Update Admin Kelas

---
+++ src/store/admin/admin_kelas.store.jsx
@@ -0,0 +1,52 @@
+import { create } from "zustand";
+import axiosNew from "../../components/AxiosConfig";
+import { toast } from "react-toastify";
+
+export const useKelasAdmin = create((set, get) => ({
+  kelas: [],
+  isLoading: false,
+
+  getDataKelas: async () => {
+    set({ isLoading: true });
+    await axiosNew
+      .get(`/kelas`, {
+        headers: {
+          "x-access-token": localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          set({ isLoading: false });
+          set({ kelas: res.data.data });
+        }
+      });
+  },
+
+  submitKelas: async (guru_id, nomor_kelas, jumlah_orang) => {
+    set({ isLoading: true });
+
+    await axiosNew
+      .post(
+        "/admin/create-kelas",
+        {
+          guru_id: guru_id,
+          nomor_kelas: nomor_kelas,
+          jumlah_orang: jumlah_orang,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "x-access-token": localStorage.getItem("token"),
+          },
+        }
+      )
+      .then((res) => {
+        if (res.status === 200 || res.status === 201) {
+          getDataKelas();
+        }
+      })
+      .catch((err) =>
+        toast.error(err.response.data.message ?? "Something went wrong")
+      );
+  },
+}));
